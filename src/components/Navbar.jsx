754aae08a9b046b13b3da5f36eb527a80ad074d1
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-25 21:22:35 +0700

    save

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,17 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav>
-        <div>
-            <a href="#">ReactTailwind</a>
-        </div>
+    <nav className='shadow-md'>
+        <div className='container mx-auto'>
+            <div>
+                <a href="#">ReactTailwind</a>
+            </div>
 
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="#">Service</a></li>
-            <li><a href="#">Contact</a></li>
-        </ul>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Service</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </div>
+        
     </nav>
   )
 }
