4aa414ba02714eb2a106ab8d39d6975b76fb71f9
SarawutCoding <sarawutphuangsumalee@gmail.com>
2026-04-26 20:15:20 +0700

    save

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const MainPage = () => {
   return (
-    <main className=''>
-        <div className=''>
-
+    <main className='container mx-auto'>
+        <div className='flex flex-row p-3 bg-amber-200'>
+            <h1 className='text-2xl'>
+                สวัสดี ผม สราวุฒิ พวงสุมาลี
+            </h1>
+            <img src="https://pbs.twimg.com/profile_images/1445742536706846733/1KpfF0n-_400x400.jpg" alt="" />
         </div>
     </main>
   )
